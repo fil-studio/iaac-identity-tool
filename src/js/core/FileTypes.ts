350cd77e6ec2ec993339138c8c429c343bc5ba19
Edu Prats <eduprats@gmail.com>
2024-09-03 13:50:39 +0200

    File name label

--- src/js/core/FileTypes.ts
+++ src/js/core/FileTypes.ts
@@ -3,15 +3,22 @@ import { addFileDropHandler } from "@fils/utils";
 export const supportedVideo = ["video/mp4", "video/webm"];
 export const supportedImage = ["image/jpeg", "image/png", "image/webp", "image/svg+xml"];
 
+export function updateVisualFileName(file:File) {
+    const i = document.querySelector('span.jsFileName');
+    i.textContent = file.name;
+}
+
 export function getVisualURL(file:File, callback:Function) {
     if(supportedVideo.indexOf(file.type) > -1) {
         // console.log('Supported video type!')
         const url = URL.createObjectURL(file);
         callback(url, true);
+        updateVisualFileName(file);
     } else if(supportedImage.indexOf(file.type) > -1) {
         // console.log('Supported image type!');
         const url = URL.createObjectURL(file);
         callback(url);
+        updateVisualFileName(file);
     } else {
         console.warn('File Type not compatible!');
     }
