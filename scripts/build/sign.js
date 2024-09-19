f08f1e0c78a9b40ba72ac7f679c5ffb326926456
Edu Prats <eduprats@gmail.com>
2024-09-19 12:50:54 +0200

    Added signing by platform on mac

--- scripts/build/sign.js
+++ scripts/build/sign.js
@@ -7,5 +7,11 @@ const id = process.env.APPLE_TEAM_ID;
 const app = 'iaac';
 
 console.log('Signing MacOs Apps with Developer ID:', id, '...');
-execSync(`codesign --force --deep --sign "${id}" packaged/${app}-darwin-arm64/${app}.app`);
-execSync(`codesign --force --deep --sign "${id}" packaged/${app}-darwin-x64/${app}.app`);
+
+if(process.argv[2] && process.argv[2].indexOf('intel') > -1) {
+    console.log('Processing intel version...')
+    execSync(`codesign --force --deep --sign "${id}" packaged/${app}-darwin-x64/${app}.app`);
+} else {
+    console.log('Processing apple silicon version...')
+    execSync(`codesign --force --deep --sign "${id}" packaged/${app}-darwin-arm64/${app}.app`);
+}
